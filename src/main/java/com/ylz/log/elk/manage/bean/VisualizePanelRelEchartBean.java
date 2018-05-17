7586ea5d628a4bb39689eacfec3c2b5aad920dbd
hxiugangxh <623145066@qq.com>
2018-05-17 09:59:40 +0800

    .

--- src/main/java/com/ylz/log/elk/manage/bean/VisualizePanelRelEchartBean.java
+++ src/main/java/com/ylz/log/elk/manage/bean/VisualizePanelRelEchartBean.java
@@ -3,12 +3,15 @@ package com.ylz.log.elk.manage.bean;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
 @Entity
 @Table(name = "cm_visualize_panel_rel_echart")
 public class VisualizePanelRelEchartBean {
+    @Id @GeneratedValue
     private Integer panelId;
     private Integer echartId;
     private Integer sort;
