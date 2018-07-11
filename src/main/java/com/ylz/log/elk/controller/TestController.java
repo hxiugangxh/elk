85290594e141f8050f90b828f7a20865bb7eda34
huxiugangxh <15759192931a>
2018-07-11 14:24:17 +0800

    .

--- src/main/java/com/ylz/log/elk/controller/TestController.java
+++ src/main/java/com/ylz/log/elk/controller/TestController.java
@@ -1,8 +1,6 @@
-/*
 package com.ylz.log.elk.controller;
 
-import com.ylz.log.elk.manage.service.IndexService;
-import org.elasticsearch.action.bulk.BulkAction;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
@@ -10,38 +8,39 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.Scroll;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.hibernate.bytecode.internal.javassist.BulkAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 @RestController
 public class TestController {
 
-
     @Autowired
     private Client client;
 
-    @Autowired
-    private IndexService indexService;
+    @GetMapping("/modifyEsData")
+    public String modifyEsData(
+            @RequestParam(value = "index",defaultValue = "") String index,
+            @RequestParam(value = "from", defaultValue = "0") Integer from,
+            @RequestParam(value = "size", defaultValue = "100") Integer size,
+            @RequestParam(value = "hostName", defaultValue = "localhost") String hostName
+            ) {
 
-    @GetMapping("/test")
-    public String index() {
+        index = "ylz--2018.05.16";
+        from = 13000;
+        size = 21231;
+        hostName = "relh5";
 
-        List<String> indexList = indexService.getRelIndex("易联众日志");
-
-        Integer page = 1;
-        Integer pageSize = 6000;
-
-        SearchRequestBuilder searchRequestBuilder = this.client.prepareSearch("ylz--2018.05.16")
-                .setFrom(10000).setSize(2341);
+        SearchRequestBuilder searchRequestBuilder = this.client.prepareSearch(index)
+                .setFrom(from).setSize(size);
 
         searchRequestBuilder.setQuery(QueryBuilders.matchAllQuery());
         SearchHits hits = searchRequestBuilder
@@ -53,24 +52,25 @@ public class TestController {
             idList.add(hit.getId());
         }
 
+        BulkRequestBuilder bulkRequestBuilder = this.client.prepareBulk();
         for (String id : idList) {
-            UpdateRequest updateRequest = new UpdateRequest("ylz--2018.05.16", "doc", id);
+            UpdateRequest updateRequest = new UpdateRequest(index, "doc", id);
 
             try {
                 XContentBuilder builder = XContentFactory.jsonBuilder().startObject();
 
-                builder.field("host", "rhel8");
+                builder.field("host", hostName);
                 builder.endObject();
                 updateRequest.doc(builder);
-                UpdateResponse updateResponse = this.client.update(updateRequest).get();
-                System.out.println(updateResponse);
+                bulkRequestBuilder.add(updateRequest);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-        return "1232131";
+        bulkRequestBuilder.execute().actionGet();
+
+        return "update success";
     }
 
 }
-*/
