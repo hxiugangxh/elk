7f2b4a704b957a3aa07f1ade3e4861d62d9817c1
hxiugangxh <623145066@qq.com>
2018-08-27 12:26:08 +0800

    .

--- src/main/java/com/ylz/log/elk/base/util/LoginInfoUtil.java
+++ src/main/java/com/ylz/log/elk/base/util/LoginInfoUtil.java
@@ -1,20 +1,20 @@
 package com.ylz.log.elk.base.util;
 
-//import org.apache.shiro.SecurityUtils;
-//import org.apache.shiro.subject.Subject;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 
 import java.security.Principal;
 
 public class LoginInfoUtil {
 
     public static String getUserName() {
-        String userName = "";
-       /* Subject subject = SecurityUtils.getSubject();
+        String userName = "admin";
+        Subject subject = SecurityUtils.getSubject();
         if (null != subject) {
             Principal principal = (Principal) subject.getPrincipal();
 
             userName = principal.getName();
-        }*/
+        }
 
         return userName;
     }
